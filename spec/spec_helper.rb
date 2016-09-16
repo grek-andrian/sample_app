d38e6da2c7aee80bc6f602c537a269bacd33c69e
grek-andrian <gretsko.andrian@gmail.com>
2016-09-16 23:43:36 +0300

    Add config for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,4 +18,7 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
   config.order = "random"
+
+  config.include Rails.application.routes.url_helpers
+
 end
